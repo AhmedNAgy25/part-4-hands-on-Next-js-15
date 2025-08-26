df5b6b931b48a3cf1199c7af5ecb51a3622065c7
Ahmed NAgy <ahmednagy25t@gmail.com>
2025-08-26 16:06:22 +0300

    fetch data with client component [....OPS >>>reverse commets wih previous one]

---
+++ src/app/users-server/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+export default async function page() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await response.json();
+  console.log(users);
+
+  return (
+    <ul className="space-y-4 p-4">
+      {users.map((user) => (
+        <li
+          key={user.id}
+          className="bg-white p-4 rounded-lg shadow-md text-gray-700"
+        >
+          <div className="font-bold">{user.name}</div>
+          <div className="text-sm">
+            <div>Username: {user.username}</div>
+            <div>Email: {user.email}</div>
+            <div>Phone: {user.phone}</div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
